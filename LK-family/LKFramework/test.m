fa868a4b69a5f8efddd751939de559812694de06
Jeremy, Yen <r00b46024@ntu.edu.tw>
2016-11-04 16:18:30 -0400

    add camera

--- LK-family/LKFramework/test.m
+++ LK-family/LKFramework/test.m
@@ -2,28 +2,24 @@
 clc;
 clear all;
 load('../../data/frames_480p_fps30.mat');
-%img = frames(:,:,1);
-%%
-% Example code for Assignment 3 in 16423
-rgb = imread('affine_prince.jpg'); 
-img = rgb2gray(rgb); 
 
-% Choose to scale the image down
-sc = 1.0; 
-
+%% Pre-processing
+img = frames(:,:,1);
+%img = rgb2gray(rgb); 
+%rgb = imread('affine_prince.jpg'); 
 [pt_X, pt_Y] = getpts(get(imshow(img),'Parent'));
  
 % 2D projected points
 %pts = sc*[771, 1587, 771, 1587; 885, 885, 1503, 1503]; for book example
 pts = [pt_X';pt_Y'];
- 
-% Height and width of the template
-dsize = [30,30]; 
-   
-% Set the template points (in order that points appear in image)
-tmplt_pts = [0, dsize(2)-1, 0, dsize(2)-1; 
-             0, 0, dsize(1)-1, dsize(1)-1]; 
 
+% Height and width of the template
+dsize = [max(pt_Y) - min(pt_Y), max(pt_X) - min(pt_X)]; 
+% Set the template points (in order that points appear in image)
+tmplt_pts = [0, dsize(2)-1, 0, dsize(2)-1; ...
+             0, 0, dsize(1)-1, dsize(1)-1];    
+
+%% Test   
 % Step 1. Extract the Template
 a = Affine; % Define an Affine object
 gnd_p = a.fit(tmplt_pts, pts); % Get the ground-truth warp
@@ -31,13 +27,13 @@ T_0 = a.imwarp(img, gnd_p, dsize); % Define the template
 clf; imagesc(img); colormap('gray'); axis off; axis image; % Display the image
 a.draw(gnd_p, dsize,'r-','LineWidth',2); % Draw the ground-truth
 
-% Step 2. Perturb the warp 
+%%
+% Step 2. Perturb the warp
 dp = [0.26, 0.26, -18.15, -0.62, -0.36, -2.32]'; 
 noise_p = gnd_p + dp; 
 h = a.draw(noise_p, dsize,'g-','LineWidth',2); % Draw the ground-truth
 
 % Step 3. Apply the LK algorithm
-%lk = LK(T_0); 
 lk = LK_IC(T_0);
 lk.fit(img, noise_p, 30, h); % Append h here to draw the result on the image
 
---
+++ camera/camera.m
@@ -0,0 +1,27 @@
+%% Set up camera
+webcamlist;
+cam = webcam;
+%% Preview
+preview(cam);
+
+%% Loop
+for idx = 1:100
+   % Acquire a single image.
+   rgbImage = snapshot(cam);
+
+   % Convert RGB to grayscale.
+   grayImage = rgb2gray(rgbImage);
+
+   % Find circles.
+   [centers, radii] = imfindcircles(grayImage, [60 80]);
+
+   % Display the image.
+   imshow(rgbImage);
+   hold on;
+   viscircles(centers, radii);
+   drawnow
+end
+
+
+%% Quit
+clear('cam');
