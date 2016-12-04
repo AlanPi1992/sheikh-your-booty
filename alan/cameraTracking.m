81dcb71a86f21be5ba515918083e63fb2d25d33f
Alan <xpi@andrew.cmu.edu>
2016-12-03 23:30:42 -0500

    add demo result to poster

--- alan/cameraTracking.m
+++ alan/cameraTracking.m
@@ -24,9 +24,9 @@ videoPlayer = vision.VideoPlayer('Position', [5 5 [frameSize(2), frameSize(1)]+5
 alpha = vision.AlphaBlender('Operation', 'Binary mask', 'MaskSource', 'Input port');
 
 % Read the image to be blended into vedio
-mickey = imread('annonymous.jpg');
+% mickey = imread('annonymous.jpg');
 % mickey = imread('emoji.png');
-% mickey = imread('baoman.png');
+mickey = imread('baoman.png');
 if (size(mickey, 3) == 1)
     mickey = repmat(mickey, [1,1,3]);
 end
